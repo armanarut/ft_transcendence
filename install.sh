6ba52fd1fe30c40633aa142ab2a480c252e0d2df
Mikhayil <mikhayil.arzumanyan@gmail.com>
2023-03-20 16:35:18 +0400

    start

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!bin/sh
 
-npm i --save-dev webpack webpack-cli webpack-dev-server css-loader html-webpack-plugin mini-css-extract-plugin ts-loader
-npm i react react-dom
-npm i --save-dev @types/react @types/react-dom
+# npm i --save-dev webpack webpack-cli webpack-dev-server css-loader html-webpack-plugin mini-css-extract-plugin ts-loader
+# npm i react react-dom
+# npm i --save-dev @types/react @types/react-dom
 npm run build
 npx serve dist
+npm start
