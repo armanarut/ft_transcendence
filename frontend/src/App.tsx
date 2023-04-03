22312cf5ce98c5b3c8fc689b684c751bd118e63a
Mikhayil <mikhayil.arzumanyan@gmail.com>
2023-04-03 19:03:03 +0400

    I'm go to home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Login from './components/Login';
 import imageToAdd from '../images/PingPong_1200x630.jpeg'
 
 
+
 function App() {
   return (
     <h1 className="container mx-auto mix-w-2xl pt-5 text-2xl font-bold ">
