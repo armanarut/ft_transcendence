5fab1c2cadde168863f2f6c6f2bcd9e2b2bc6055
Mikhayil <mikhayil.arzumanyan@gmail.com>
2023-04-05 19:45:45 +0400

    past image on main page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,19 +2,18 @@ import React from 'react';
 import './App.css';
 import Nikname from './components/Nikname';
 import Login from './components/Login';
-import imageToAdd from '../images/PingPong_1200x630.jpeg'
-
-
+import aaa from "./images/pong.jpg"
 
 function App() {
   return (
-    <h1 className="container mx-auto mix-w-2xl pt-5 text-2xl font-bold ">
-      <a href="#" className="text-1xl mx-11 font-semibold leading-7 text-gray-900">Login</a>
-      <a href="#" className="text-1xl mx-11 font-semibold leading-7 text-gray-900">Contacts</a>
-      <a href="#" className="text-1xl mx-11 font-semibold leading-7 text-gray-900">THE GAME</a>
-      <a href="#" className="text-1xl mx-11 font-semibold leading-7 text-gray-900">Chanels</a>
-      <a href="#" className="text-1xl mx-11 font-semibold leading-7 text-gray-900">Nikname</a>
+    <h1 className="sm:text-center container pt-5 text-2xl font-bold">
 
+      <a href="#" className="px-10 text-1xl mx-14 font-semibold leading-7 text-gray-900">Login</a>
+      <a href="#" className="px-10 text-1xl mx-14 font-semibold leading-7 text-gray-900">Contacts</a>
+      <a href="#" className="px-10 text-1xl mx-14 font-semibold leading-7 text-gray-900">THE GAME</a>
+      <a href="#" className="px-10 text-1xl mx-14 font-semibold leading-7 text-gray-900">Chanels</a>
+      <a href="#" className="px-10 text-1xl mx-14 font-semibold leading-7 text-gray-900">Nikname</a>
+      <img className="px-28 pt-6 absalute left-0 top-0 w-full h-full z-10" src={aaa} alt="" height="600px" width="800px" ></img>
     </h1>
   );
 }
