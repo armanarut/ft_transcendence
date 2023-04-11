813d016e6181bf805e43abf76315ce384cbb5d41
Mikhayil <mikhayil.arzumanyan@gmail.com>
2023-04-11 20:11:35 +0400

    fudomental change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css";
-import { Home } from './components/Home';
-import { Nikname } from "./components/Nikname";
-import { Login } from "./components/Login";
+import Home from './components/Home';
+import Profile from "./components/Profile";
+import Login from "./components/Login";
 import { Contacts } from "./components/Contacts";
 import { TheGame } from "./components/TheGame";
 import { Chanels } from "./components/Chanels";
@@ -18,7 +18,7 @@ function App() {
             <Route path="/contacts" element={<Contacts />} />
             <Route path="/thegame" element={<TheGame />} />
             <Route path="/chanels" element={<Chanels />} />
-            <Route path="/nikname" element={<Nikname />} />
+            <Route path="/profile" element={<Profile />} />
           </Routes>
        </Router>
   )
--- frontend/src/components/Nikname.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react"
-
-
-export function Nikname() {
-    return <h1>Nikname</h1>
-}
