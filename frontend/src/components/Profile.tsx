17ae656f3df9f0f61cc6d7ff47c49f3ac4bc3e37
Mikhayil <mikhayil.arzumanyan@gmail.com>
2023-04-27 18:44:53 +0400

    start create Profile

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -1,13 +1,19 @@
 import React from "react"
 import { Layout } from "./Layout";
+import photo from '@SRC_DIR/assets/images/pong.jpg';
 
 
 const Profile = () => {
     return (
         <Layout>
-            <div className="">
-                <div>
+            <div className="mt-8 relative w-full h-screen">
+                <img className="absolute w-full h-full object-cover mix-blend-overlay" src={photo} alt="/" />
+
+            <div className="grid place-items-center h-screen">
+                <div className="rounded=3xl p-6 w-[450] text-white h-[250] bg-[#171822]">
+                    Hi
                 </div>
+            </div>
             </div>
         </Layout>
     )
