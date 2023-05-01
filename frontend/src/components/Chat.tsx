a8cd94fb21cc16a7ae2c78e993ceba75bfab47f8
Mikhayil <mikhayil.arzumanyan@gmail.com>
2023-05-01 18:42:31 +0400

    creat SignIn page, Login page rename on Chat and delete Registration page

---
+++ frontend/src/components/Chat.tsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+import { Layout } from './Layout'
+import { Logo } from './Logo'
+import { Link, useLocation } from 'react-router-dom'
+import photo from '@SRC_DIR/assets/images/pong.jpg';
+import { instance } from "./utils"
+
+const Chat = () => {
+    return ( 
+        <h1>Contacts</h1>
+        );
+    };
+
+export default Chat;
